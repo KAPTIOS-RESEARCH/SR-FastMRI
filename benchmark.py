db5678e9b944a6244a80c7c0f23fc5ec067d175e
bniepce <brad.niepce@kaptios.com>
2025-03-21 08:57:31 +0100

    Fix quantization

--- benchmark.py
+++ benchmark.py
@@ -18,17 +18,18 @@ if __name__ == "__main__":
     parser.add_argument("--fastMRI_data_path", type=str, required=True)
     parser.add_argument("--model_path", type=str, required=True)
     parser.add_argument("--quantized_model_path", type=str, required=True)
+    parser.add_argument("--n_data_samples", type=int, default=50)
     args = parser.parse_args()
     non_quantized_session = InferenceSession(args.model_path)
     quantized_session = InferenceSession(args.quantized_model_path)
     logging.info('Inference sessions loaded ✅')
     dataset = FastMRISuperResolutionDataset(args.fastMRI_data_path)
-    dataset = Subset(dataset, range(2))
+    dataset = Subset(dataset, range(args.n_data_samples))
     
-    t1 = run_session_inference(non_quantized_session, dataset)
-    t2 = run_session_inference(quantized_session, dataset)
-    
-    logging.info(f"🟢 Non-Quantized Model | Avg Inference Time: {t1:.2f} ms")
-    logging.info(f"🔥 Quantized Model | Avg Inference Time: {t2:.2f} ms")
+    t1, psnr1 = run_session_inference(non_quantized_session, dataset)
+    t2, psnr2 = run_session_inference(quantized_session, dataset)
+
+    logging.info(f"🟢 Non-Quantized Model | Avg Inference Time & PSNR : {t1:.2f} ms; {psnr1:.2f}")
+    logging.info(f"🔥 Quantized Model | Avg Inference Time & PSNR : {t2:.2f} ms; {psnr2:.2f}")
     logging.info(f"🚀 Speedup: {t1 / t2:.2f}x faster with quantization")
 
--- src/utils/inference.py
+++ src/utils/inference.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from onnxruntime import InferenceSession
 from tqdm import tqdm
+from torchmetrics.image import PeakSignalNoiseRatio
 
 def run_session_inference(session: InferenceSession, dataset: torch.utils.data.Dataset):
     """Run inference of an onnx session on all samples in the given dataset
@@ -14,17 +15,21 @@ def run_session_inference(session: InferenceSession, dataset: torch.utils.data.D
     Returns:
         tuple: The average time of inference in seconds, the average PSNR
     """
+    psnr = PeakSignalNoiseRatio()
     times = []
+    psnr_values = []
     input_name = session.get_inputs()[0].name
     output_name = session.get_outputs()[0].name
     with tqdm(total=len(dataset), desc=f'Running inference on {len(dataset)} samples ...') as pbar:
         for idx, sample in enumerate(dataset):
-            x, _ = sample.image, sample.target
-            x = x.numpy()
+            x, y = sample.image.numpy(), sample.target.numpy()
             x = np.expand_dims(x, axis=0)
+            y = np.expand_dims(y, axis=0)
             start_time = time.perf_counter()
-            session.run([output_name], {input_name: x})[0]
+            output = session.run([output_name], {input_name: x})[0]
+            psnr_values.append(psnr(torch.Tensor(output), torch.Tensor(y)))
             times.append(time.perf_counter() - start_time)
             pbar.update(1)
     avg_time_sec = np.mean(times)
-    return avg_time_sec
+    avg_psnr = np.mean(psnr_values)
+    return avg_time_sec, avg_psnr
